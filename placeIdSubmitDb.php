099e854886a696f9b777cbd7c61a2038fad629b0
jackbourkemckenna <jackbourkemckenna@gmail.com>
2017-03-05 22:57:50 +0000

    restored code for db connection Only for testing use- JBM

--- placeIdSubmitDb.php
+++ placeIdSubmitDb.php
@@ -1,14 +1,24 @@
 <?php 
 session_start();
-include_once('dbQueries/placeId.php');
+
 $placeValue = $_GET['place'];
 if (!isset($_SESSION['userSession'])) {
  header("Location: index.php");
 }
 
 
-
-
+// Tempory code for place submit
+$servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "pubgo";
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    $sql = "update pubUsers set place_id ='$placeValue' where pub_id=".$_SESSION['userSession'];
+    $result = $conn->query($sql);
+    
     
     if($sql){
        header("Location: https://pubgo-jackbourkemckenna.c9users.io/dashboard.php");
@@ -20,6 +30,5 @@ if (!isset($_SESSION['userSession'])) {
         echo 'error found while submiting to database';
         
     }
-    
 
 ?>
